5d6921c34271a81d109f06ab6df44dfcb10f015a
Sanjay Sharma <sanjupunj16@gmail.com>
2019-08-27 08:26:27 +0000

    commit

---
+++ Scratch/readtxtfile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Specify the path of the secret.txt file
+secrettxtfile=/home/student/CSI6203/Scratch/secret.txt
+# with condition check 
+if test -f "$secrettxtfile"; then
+echo " secret.txt file exists and contents are :"
+cat secret.txt
+else
+echo "This is a different File"
+fi
